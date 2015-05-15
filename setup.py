b84c48926cc259dbc9e6eb0d50832b76cbd5b97f
Adam Israel <adam@adamisrael.com>
2015-05-15 14:46:02 -0400

    Fix console script and update url

--- setup.py
+++ setup.py
@@ -14,17 +14,17 @@ tests_require = [
 
 setup(
     name='charm-benchmark',
-    version='0.0.1',
+    version='0.0.2',
     description='Library to aid in the creation of benchmark actions in Juju',
     install_requires=install_requires,
-    url="https://launchpad.net/~cabs-team",
-    packages=['charmbenchmark'],
+    url="https://github.com/juju-solutions/charm-benchmark",
+    packages=['charmbenchmark', 'charmbenchmark.cli'],
     entry_points={
         'console_scripts': [
-            'benchmark-start=charmbenchmark.cli.start:main',
-            'benchmark-finish=charmbenchmark.cli.finish:main',
-            'benchmark-actions=charmbenchmark.cli.actions:main',
-            'benchmark-composite=charmbenchmark.cli.composite:main',
+            'benchmark-start = charmbenchmark.cli.start:main',
+            'benchmark-finish = charmbenchmark.cli.finish:main',
+            'benchmark-actions = charmbenchmark.cli.actions:main',
+            'benchmark-composite = charmbenchmark.cli.composite:main',
         ]
     }
 )
